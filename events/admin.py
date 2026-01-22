06bcdfbe61c4b4e0304c75614836482fff2d2c31
joaovictorSsouza <joaovictor.souzacontato@gmail.com>
2026-01-22 15:41:13 -0300

    Configurei o admin.py para cadastro pelo admin e configurei o null, blank em algumas colunas do banco de dados.

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Event, Categories
 
-# Register your models here.
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'categories',)
+    search_fields = ('id',)
+
+@admin.register(Event)
+class Event(admin.ModelAdmin):
+    list_display = ('event_name', 'description',
+                    'date', 'location', 'is_remote',
+                    'capacity', 'price', 'category',)
+    search_fields = ('event_name', 'date',)
+
--- events/models.py
+++ events/models.py
@@ -14,13 +14,13 @@ class Event(models.Model):
     id = models.AutoField(primary_key=True)
     event_name = models.CharField(max_length=200)
     #Colocar depois o help_text= para explicar
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     date = models.DateTimeField()
-    location = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, null=True, blank=True)
     is_remote = models.BooleanField(default=False)
-    capacity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='category_list')
+    capacity = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='category_list', null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_user')
     created_at = models.DateTimeField(auto_now_add=True)
 
