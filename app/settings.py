55e4fe9b1d6cc212aea60b1b234156db3d9e9544
joaovictorSsouza <joaovictor.souzacontato@gmail.com>
2026-01-25 20:37:40 -0300

    Colocando o bnaco de dados na nuvem com o postgresql - nano

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,14 +70,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'events',
-        'USER': 'postgres',
-        'PASSWORD': 'rosan@JV077',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True
+    )
 }
 
 
